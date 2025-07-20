9dd60684f1d96fb68e31a85aa3fefd1846d96321
Lukáš Vrba <lukas.vrba@eu.panasonic.com>
2025-07-20 18:07:16 +0200

    Pipelines testing Python

---
+++ .github/Test.py
@@ -0,0 +1,12 @@
+import unittest
+import main
+
+class TestMultiplication(unittest.TestCase):
+    def test_multiplication_integers(self):
+        self.assertEqual(multiplication(2,3), 6)
+
+    def test_multiplication_floats(self):
+        self.assertEqual(multiplication(2.5,4), 10.0)
+
+if __name__ == '__main__':
+    unittest.main(2,3)
